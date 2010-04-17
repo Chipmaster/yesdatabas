6b9912d7ef6abdf618c68b00dfbed09f7fbba122
Galeru <Galeru@8887b15e-1689-11df-acba-0d0957b32f48>
2010-04-17 20:35:41 +0000

    Added schema/models for custom data fields.

--- trunk/app/models/user.rb
+++ trunk/app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
 	validates_uniqueness_of :email
 	validates_confirmation_of :password
 	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
+  has_many :data_fields, :dependent => :delete_all
 end
