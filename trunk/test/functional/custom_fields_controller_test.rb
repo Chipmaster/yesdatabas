d32f2f0fe8bb46846268a46d7daee882358a68e2
Galeru <Galeru@8887b15e-1689-11df-acba-0d0957b32f48>
2010-04-21 22:36:40 +0000

    Trying to commit again.

--- trunk/test/functional/custom_fields_controller_test.rb
+++ trunk/test/functional/custom_fields_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class FieldTypeTest < ActiveSupport::TestCase
+class CustomFieldsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
