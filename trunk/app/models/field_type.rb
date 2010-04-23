c3fc2b6acfe1430b8b40cf65f3818e380fe5f547
Galeru <Galeru@8887b15e-1689-11df-acba-0d0957b32f48>
2010-04-23 16:54:25 +0000

    Added a fields controller that allows CRUD on custom fields.

--- trunk/app/models/field_type.rb
+++ trunk/app/models/field_type.rb
@@ -1,3 +1,9 @@
 class FieldType < ActiveRecord::Base
   has_many :data_fields
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
+  def all_fields
+    FieldType.find(:all)
+  end
 end
