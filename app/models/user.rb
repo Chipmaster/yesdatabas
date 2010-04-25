15132a1b5ca0766edd1041bf78650fc63175298b
Galeru <Galeru@8887b15e-1689-11df-acba-0d0957b32f48>
2010-04-25 20:40:59 +0000

    DB Migrations and models fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,5 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :password_hash
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
-  has_many :data_fields, :dependent => :delete_all
+  has_many :data_fields
 end
