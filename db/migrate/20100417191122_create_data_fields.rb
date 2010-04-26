1baa7e858a452870c5428611ad24fa2b36ed7b32
Galeru <Galeru@8887b15e-1689-11df-acba-0d0957b32f48>
2010-04-26 19:39:00 +0000

    Fixed to allow for Mike to use a diagrammer.

--- db/migrate/20100417191122_create_data_fields.rb
+++ db/migrate/20100417191122_create_data_fields.rb
@@ -1,6 +1,6 @@
 class CreateDataFields < ActiveRecord::Migration
   def self.up
-    create_table :data_field do |t|
+    create_table :data_fields do |t|
       t.string :value
       t.references :user
       t.references :field_type
