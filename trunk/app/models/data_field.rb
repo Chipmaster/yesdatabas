d1cbe30babca5079615192314be5e1af2e8cc574
Galeru <Galeru@8887b15e-1689-11df-acba-0d0957b32f48>
2010-04-27 02:41:34 +0000

    Add UI for custom fields.

--- trunk/app/models/data_field.rb
+++ trunk/app/models/data_field.rb
@@ -1,4 +1,11 @@
 class DataField < ActiveRecord::Base
   belongs_to :user
   belongs_to :field_type
+
+  def self.create(value, user, fieldtype)
+    @field = DataField.new
+    @field.value = value
+    user << @field
+    fieldtype << @field
+  end
 end
