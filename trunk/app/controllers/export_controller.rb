9eb4d186b3a2bcbc52148fa278a201b9ff6bf303
djmeteyer <djmeteyer@8887b15e-1689-11df-acba-0d0957b32f48>
2010-05-05 05:18:43 +0000

    finished export, if you guys changed much since Sunday, we may need to revert..

--- trunk/app/controllers/export_controller.rb
+++ trunk/app/controllers/export_controller.rb
@@ -1,27 +1,53 @@
 class ExportController < ApplicationController
 	# require 'rubygems' if using this outside of Rails
 require 'fastercsv'
-
+	
+	#  This is the routine called when the "export to csv" button is pressed
+	#  Depending on the values passed in by the checkbox, appropriate information will
+	#  be output.  
 	def dump_csv
+	
+		#  The 'master' list of fields is retrived from the first user, ordered by usertype.  This can be done
+		#  because all users will have every field, so the field information will be the same.
+		#  A string with filename information is also declared.
 	  @users = User.find(:all, :order => "Usertype")
 	  @user = User.find(:first);
 	  @outfile = "members_" + Time.now.strftime("%m-%d-%Y") + ".tsv"
 	  @fieldtypes = FieldType.find(:all)
 	  fieldnames = []
-	  #@datafield.each do |df|
-		#if(df.value)
-		#	fieldnames.push(df.id)
-		#end
-	  #end
-	  fieldnames.push("Last Name" + "\t")
-	  fieldnames.push("First Name"+ "\t")
-	  fieldnames.push("Email"+ "\t")
-	  fieldnames.push("Usertype"+ "\t")
-	  @fieldtypes.each do |field| 
-		fieldnames.push(field.name + "\t")
-	  end
-		
 	 
+		#  params[:field_id] contains information retreived from the checkboxes.  If the 
+		#  value is 1, then the box is checked and the field will be added to fieldnames.
+		#  fieldnames is a list that will be output to the csv file via the csv << [] command.
+	  if(params[:lastname].to_i == 1)
+		fieldnames.push("Last Name" + "\t")
+	  end
+	  if(params[:firstname].to_i == 1)
+		fieldnames.push("First Name"+ "\t")
+	  end
+	  if(params[:email].to_i == 1)
+		fieldnames.push("Email"+ "\t")
+	  end
+	  if(params[:usertype].to_i == 1)
+		fieldnames.push("Usertype"+ "\t")
+	  end
+	  
+	  #  All the custom fields need to be checked a little differently.
+	  #  Because any duplicate field ID is ignored, I was forced to use a hash.
+	  #  Syntactically, this means a slight difference in where the values are.
+	  #  Values are stored in params[:custom][:field_id]
+	  #  This same logic is applied when drawing on the actual data from a user.
+	  
+	  @fieldtypes.each do |field| 
+		a = []
+		a = params[:custom]
+		if(a != nil)
+			if(params[:custom]["#{field.name}"].to_i == 1)
+				fieldnames.push(field.name + "\t")
+			end
+		end
+	  end
+	  
 	  csv_data = FasterCSV.generate do |csv|
 		csv << [
 			fieldnames.each do |field|
@@ -29,28 +55,39 @@ require 'fastercsv'
 			end
 		]
 		
+		#  For each user, each param is checked to see if it's associated data needs to be output.
+		#  If it needs to be output, it is added to data[] and then data[] is output to a csv.
 		@users.each do |user|
 			data = []
-			data.push(user.lastname + "\t")
-			data.push(user.firstname + "\t")
-			data.push(user.email + "\t")
-			data.push(user.usertype + "\t")
+			if(params[:lastname].to_i == 1)
+			  data.push(user.lastname + "\t")
+			end
+			if(params[:firstname].to_i == 1)
+			  data.push(user.firstname + "\t")
+			end
+			if(params[:email].to_i == 1)
+			  data.push(user.email + "\t")
+			end
+			if(params[:usertype].to_i == 1)
+			  data.push(user.usertype + "\t")
+			end
 			@fieldtypes.each do |field|
-				temp = field.data_fields.find(:first, :conditions => {:user_id => user.id})
-				if temp != nil
-					data.push(temp.value + "\t")
-				else
-					data.push("Empty" + "\t")
+				a = []
+				a = params[:custom]
+				if(a != nil)
+					if(params[:custom]["#{field.name}"].to_i == 1)
+						temp = field.data_fields.find(:first, :conditions => {:user_id => user.id})
+						if temp != nil
+							data.push(temp.value + "\t")
+						else
+							data.push("Empty" + "\t")
+						end
+					end
 				end
 			end
 			csv << [
 				data
 			]
-			#@fieldtypes.each do |field| 
-			#	csv << [ 
-			#		user.field.name
-			#	]
-			#end	
 	
 		end
 	  end
@@ -62,6 +99,7 @@ require 'fastercsv'
 	  flash[:notice] = "Export complete!"
 	end
 	
+	#  The Index method displays the index.html page, which has a list of field names and checkboxes.
 	def index
     @user = User.find(:first);
     if (session[:id].to_s == params[:id].to_s) || session[:adminuser]
